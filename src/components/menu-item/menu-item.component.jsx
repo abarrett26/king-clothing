ab149d89bb34eb2f025f5173faf854d3337fb516
Alexander Barrett <abarrett100@gmail.com>
2020-03-17 02:26:02 -0500

    third

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import { withRouter } from 'react-router-dom';
 
-const MenuItem = ({title, imageUrl, size}) => (
+const MenuItem = ({title, imageUrl, size, history}) => (
 
 
-	<div className={`${size} menu-item`}>
+	<div className={`${size} menu-item` onClick={() => history.push()}}>
 		<div 
 		className='background-image' 
 		style={{
@@ -18,4 +19,4 @@ const MenuItem = ({title, imageUrl, size}) => (
 	</div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
